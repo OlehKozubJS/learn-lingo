f4a6b2e67e255ab4468ae83407af66b9cfdb6fca
OlehKozubJS <oleg.kozub54@gmail.com>
2024-01-19 22:48:05 +0200

    Create useKeydownHandler.js

---
+++ src/useKeydownHandler.js
@@ -0,0 +1,15 @@
+//import { useEffect } from "react";
+
+const useKeydownHandler = (callbackName) => {
+  const handleKeydown = (event) => {
+    if (event.type === "keydown" && event.key === "Escape") {
+      callbackName();
+    }
+  };
+
+  window.addEventListener("keydown", handleKeydown);
+
+  return () => {
+    window.removeEventListener("keydown", handleKeydown);
+  };
+};
