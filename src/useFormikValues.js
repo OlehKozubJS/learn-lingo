bb0ea9afbd042e4201d6cbf172b692a4ac7b467b
OlehKozubJS <oleg.kozub54@gmail.com>
2024-01-21 00:16:24 +0200

    Update useFormikValues.js

--- src/useFormikValues.js
+++ src/useFormikValues.js
@@ -1,15 +1,15 @@
 import { useFormik } from "formik";
-import { useState } from "react";
+//import { useState } from "react";
 
 const useFormikValues = (initialValues) => {
-  const [values, setValues] = useState();
+  //const [values, setValues] = useState();
   const formik = useFormik({
     initialValues,
     onSubmit: (values) => {
-      setValues(values, null, 2);
+      return values;
     },
   });
-  return [formik, values];
+  return null;
 };
 
 export { useFormikValues };
