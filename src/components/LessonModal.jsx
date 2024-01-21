e3eb4cd9e03760de80e7400d0d6fa49c989016af
OlehKozubJS <oleg.kozub54@gmail.com>
2024-01-21 20:01:38 +0200

    Update LessonModal.jsx

--- src/components/LessonModal.jsx
+++ src/components/LessonModal.jsx
@@ -2,6 +2,8 @@ import { useFormik } from "formik";
 
 import { FormikForm, FormikName, FormikEmail } from "../formik-components";
 
+import ModalStyles from "./Modal.module.css";
+
 const LessonModal = ({ handleSubmit }) => {
   const formik = useFormik({
     initialValues: { reason: "", name: "", email: "", phone: "" },
