21a24968ccbba48b081bff31a62fa90b1e93ae17
OlehKozubJS <oleg.kozub54@gmail.com>
2024-01-21 23:09:49 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 
-import { useKeydown } from "./useKeydown";
+import { Modal } from "./components/Modal";
 
-import AppStyles from "./App.module.css";
+//import AppStyles from "./App.module.css";
 
 function App() {
   const [isModal, setIsModal] = useState(false);
@@ -11,30 +11,14 @@ function App() {
     setIsModal(true);
   };
 
-  const closeModal = () => {
-    setIsModal(false);
-  };
-
-  const handleBackdropClick = (event) => {
-    if (event.target.className === AppStyles.Backdrop) {
-      closeModal();
-    }
-  };
-
-  useKeydown(closeModal, "Escape");
-
   return (
     <div>
       <h1>Main Page</h1>
-      <div>{JSON.stringify(values)}</div>
       <button type="button" onClick={openModal}>
         Open Modal
       </button>
-      {isModal && (
-        <div className={AppStyles.Backdrop} onClick={handleBackdropClick}>
-          <div className={AppStyles.Modal}>This is Modal</div>
-        </div>
-      )}
+      {isModal && <Modal isOpen={isModal} />}
+      <div>Values</div>
     </div>
   );
 }
