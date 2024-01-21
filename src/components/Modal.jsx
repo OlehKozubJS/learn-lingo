9d25dd1391f0e58626a83b3d54ab213e071e5b66
OlehKozubJS <oleg.kozub54@gmail.com>
2024-01-21 23:20:38 +0200

    Update Modal.jsx

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -6,15 +6,8 @@ import { SVGImage } from "../icons";
 
 import ModalStyles from "./Modal.module.css";
 
-const Modal = ({ isOpen, children }) => {
-  const [isModalOpen, setIsModalOpen] = useState(isOpen);
+const Modal = ({ closeModal, children }) => {
   const [isHover, setIsHover] = useState(false);
-
-  const handleClick = (event) => {
-    if (event.target.dataset.close) {
-      setIsModalOpen(false);
-    }
-  };
 
   const handleMouseOver = () => {
     setIsHover(true);
@@ -23,35 +16,35 @@ const Modal = ({ isOpen, children }) => {
     setIsHover(false);
   };
 
-  const closeModal = () => {
-    setIsModalOpen(false);
+  const handleClick = (event) => {
+    if (event.target.dataset.close) {
+      closeModal(false);
+    }
   };
 
   useKeydown(closeModal, "Escape");
 
   return (
-    isModalOpen && (
-      <div
-        className={ModalStyles.Backdrop}
-        onClick={handleClick}
-        data-close={true}
-      >
-        <div className={ModalStyles.Modal}>
-          <div className={ModalStyles.ModalHeader}>
-            <button
-              className={ModalStyles.CloseButton}
-              onClick={handleClick}
-              onMouseOver={handleMouseOver}
-              onMouseLeave={handleMouseLeave}
-              data-close={true}
-            >
-              <SVGImage name={isHover ? "close-hover" : "close"} />
-            </button>
-          </div>
-          <div className={ModalStyles.ModalContent}>{children}</div>
+    <div
+      className={ModalStyles.Backdrop}
+      onClick={handleClick}
+      data-close={true}
+    >
+      <div className={ModalStyles.Modal}>
+        <div className={ModalStyles.ModalHeader}>
+          <button
+            className={ModalStyles.CloseButton}
+            onClick={handleClick}
+            onMouseOver={handleMouseOver}
+            onMouseLeave={handleMouseLeave}
+            data-close={true}
+          >
+            <SVGImage name={isHover ? "close-hover" : "close"} />
+          </button>
         </div>
+        <div className={ModalStyles.ModalContent}>{children}</div>
       </div>
-    )
+    </div>
   );
 };
 
