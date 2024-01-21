6d678ddbf049edac8e93ec7e0016ae6c498a6a95
OlehKozubJS <oleg.kozub54@gmail.com>
2024-01-21 20:09:18 +0200

    Create Modal.jsx

---
+++ src/components/Modal.jsx
@@ -0,0 +1,23 @@
+import { useFormik } from "formik";
+
+import { FormikForm, FormikName, FormikEmail } from "../formik-components";
+
+import ModalStyles from "./Modal.module.css";
+
+const LessonModal = ({ handleSubmit }) => {
+  const formik = useFormik({
+    initialValues: { reason: "", name: "", email: "", phone: "" },
+    onSubmit: (values) => {
+      handleSubmit(values);
+    },
+  });
+
+  return (
+    <FormikForm formik={formik}>
+      <FormikEmail formik={formik}>Full Name</FormikEmail>
+      <FormikEmail formik={formik}>Email</FormikEmail>
+    </FormikForm>
+  );
+};
+
+export { LessonModal };
