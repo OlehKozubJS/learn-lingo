475c0e93dd7a70f3674b91ebc36b05834eb524c4
OlehKozubJS <oleg.kozub54@gmail.com>
2024-01-26 00:39:42 +0200

    Create FormikSelect.jsx

---
+++ src/formik-components/FormikSelect.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+import { SVGImage } from "../icons";
+
+import styles from "./FormikComponents.module.css";
+
+const FormikPassword = ({ formik }) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const [isFocus, setIsFocus] = useState(false);
+
+  const showPassword = () => {
+    setIsVisible(true);
+  };
+
+  const hidePassword = () => {
+    setIsVisible(false);
+  };
+
+  const handleFocus = () => {
+    setIsFocus(true);
+  };
+
+  const handleBlur = () => {
+    setIsFocus(false);
+  };
+
+  return (
+    <label
+      htmlFor="password"
+      className={`${styles.FormikLabel} ${
+        isFocus ? styles.Focus : styles.Blur
+      }`}
+    >
+      <input
+        id="password"
+        name="password"
+        type={isVisible ? "text" : "password"}
+        placeholder="Password"
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        onChange={formik.handleChange}
+        value={formik.values.password}
+        className={styles.FormikInput}
+      />
+      <button
+        type="button"
+        onClick={isVisible ? hidePassword : showPassword}
+        className={styles.PasswordButton}
+      >
+        <SVGImage name={isVisible ? "visible" : "hidden"} size={20} />
+      </button>
+    </label>
+  );
+};
+
+export { FormikPassword };
