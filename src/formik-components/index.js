dafc29c8b847189d6e07f06b3d1dbf0b05854f03
OlehKozubJS <oleg.kozub54@gmail.com>
2024-01-26 01:21:47 +0200

    Update index.js

--- src/formik-components/index.js
+++ src/formik-components/index.js
@@ -2,6 +2,15 @@ import { FormikForm } from "./FormikForm";
 import { FormikName } from "./FormikName";
 import { FormikEmail } from "./FormikEmail";
 import { FormikPassword } from "./FormikPassword";
-import { useFormikValues } from "./useFormikValues";
+import { FormikPhone } from "./FormikPhone";
+import { FormikRadio } from "./FormikRadio";
+import {} from "./FormikSelect";
 
-export { FormikForm, FormikName, FormikEmail, FormikPassword, useFormikValues };
+export {
+  FormikForm,
+  FormikName,
+  FormikEmail,
+  FormikPassword,
+  FormikPhone,
+  FormikRadio,
+};
