bc10c3361e50e3a74124689c143207476c31f09a
OlehKozubJS <oleg.kozub54@gmail.com>
2024-01-25 20:33:37 +0200

    Update LogInModal.jsx

--- src/components/LogInModal.jsx
+++ src/components/LogInModal.jsx
@@ -1,10 +1,24 @@
+import { useFormik } from "formik";
+
 import { Modal } from "./Modal";
 import { FormikForm, FormikEmail, FormikPassword } from "../formik-components";
 
 import ModalStyles from "./Modal.module.css";
 
-const LoginModal = () => {
-  <Modal></Modal>;
+const LoginModal = ({ onSubmit }) => {
+  const formik = useFormik({
+    initialValues: { email: "", password: "" },
+    onSubmit: (values) => {
+      onSubmit(values);
+    },
+  });
+
+  <Modal closeModal={formik}>
+    <FormikForm formik={formik}>
+      <FormikEmail formik={formik} />
+      <FormikPassword formik={formik} />
+    </FormikForm>
+  </Modal>;
 };
 
 export { LoginModal };
