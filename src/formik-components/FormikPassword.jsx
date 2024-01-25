cad7d7a4b9863c1a56b32836bda0662d8fc4fc4e
OlehKozubJS <oleg.kozub54@gmail.com>
2024-01-25 13:46:19 +0200

    Update FormikPassword.jsx

--- src/formik-components/FormikPassword.jsx
+++ src/formik-components/FormikPassword.jsx
@@ -1,14 +1,41 @@
+import { SVGImage } from "../icons";
+import { useState } from "react";
+
 const FormikPassword = ({ formik, children }) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const [isFocus, setIsFocus] = useState(false);
+
+  const showPassword = () => {
+    setIsVisible(true);
+  };
+
+  const hidePassword = () => {
+    setIsVisible(false);
+  };
+
+  const handleFocus = () => {
+    setIsFocus(true);
+  };
+
+  const handleBlur = () => {
+    setIsFocus(false);
+  };
+
   return (
     <label htmlFor="password">
       <input
         id="password"
         name="password"
-        type="password"
+        type={isVisible ? "text" : "password"}
         placeholder={children}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
         onChange={formik.handleChange}
         value={formik.values.password}
       />
+      <button onClick={isVisible ? hidePassword : showPassword}>
+        <SVGImage name={isVisible ? "visible" : "hidden"} />
+      </button>
     </label>
   );
 };
