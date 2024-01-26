23cff13603cb314cd05280d2ea0c945c09e18f87
OlehKozubJS <oleg.kozub54@gmail.com>
2024-01-27 00:27:12 +0200

    set teacher filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,97 +1,29 @@
 import { useState } from "react";
 
 import { LoginModal, RegisterModal, LessonModal } from "./components";
+import { FilterSelect } from "./components/FilterSelect";
 
 import AppStyles from "./App.module.css";
 
 function App() {
-  const [isLogInModal, setIsLogInModal] = useState(false);
-  const [isSignUpModal, setIsSignUpModal] = useState(false);
-  const [isLessonModal, setIsLessonModal] = useState(false);
-  const [logInValues, setLogInValues] = useState({ email: "", password: "" });
-  const [signUpValues, setSignUpValues] = useState({
-    name: "",
-    email: "",
-    password: "",
-  });
-  const [lessonValues, setLessonValues] = useState({
-    reasonForLearning: "",
-    name: "",
-    email: "",
-    tel: "",
-  });
+  const [selectedItem, setSelectedItem] = useState("");
 
-  const openLogInModal = () => {
-    setIsLogInModal(true);
-  };
-
-  const closeLogInModal = () => {
-    setIsLogInModal(false);
-  };
-
-  const openSignUpModal = () => {
-    setIsSignUpModal(true);
-  };
-
-  const closeSignUpModal = () => {
-    setIsSignUpModal(false);
-  };
-
-  const openLessonModal = () => {
-    setIsLessonModal(true);
-  };
-
-  const closeLessonModal = () => {
-    setIsLessonModal(false);
-  };
-
-  const handleLogInSubmit = (values) => {
-    setLogInValues(values);
-  };
-
-  const handleSignUpSubmit = (values) => {
-    setSignUpValues(values);
-  };
-
-  const handleLessonSubmit = (values) => {
-    setLessonValues(values);
+  const selectItem = (item) => {
+    setSelectedItem(item);
   };
 
   return (
     <div>
       <h1>Main Page</h1>
-      <button type="button" onClick={openLogInModal}>
-        Open Log In Modal
-      </button>
-      <button type="button" onClick={openSignUpModal}>
-        Open Register Modal
-      </button>
-      <button type="button" onClick={openLessonModal}>
-        Open Lesson Modal
-      </button>
-      {isLogInModal && (
-        <LoginModal closeModal={closeLogInModal} onSubmit={handleLogInSubmit} />
-      )}
-      {isSignUpModal && (
-        <RegisterModal
-          closeModal={closeSignUpModal}
-          onSubmit={handleSignUpSubmit}
+      <div>{selectedItem}</div>
+      <div style={{ backgroundColor: "rgb(200, 200, 250)" }}>
+        <FilterSelect
+          title="Languages"
+          options={["French", "English", "German", "Ukrainian", "Polish"]}
+          select={selectItem}
+          width="221px"
         />
-      )}
-      {isLessonModal && (
-        <LessonModal
-          closeModal={closeLessonModal}
-          onSubmit={handleLessonSubmit}
-          teacherName="John Doe"
-          teacherPhoto={"https://ftp.goit.study/img/avatars/1.jpg"}
-        />
-      )}
-      <div>{logInValues.email}</div>
-      <div>{logInValues.password}</div>
-      <div>{signUpValues.name}</div>
-      <div>{signUpValues.email}</div>
-      <div>{signUpValues.password}</div>
-      <div>{JSON.stringify(lessonValues)}</div>
+      </div>
     </div>
   );
 }
--- src/components/FilterSelect.jsx
+++ src/components/FilterSelect.jsx
@@ -4,8 +4,9 @@ import { SVGImage } from "../icons";
 
 import styles from "./TeacherFilter.module.css";
 
-const FormikSelect = ({ title, options, selected, select }) => {
+const FilterSelect = ({ title, options, select, width }) => {
   const [isOptionsList, setIsOptionsList] = useState(false);
+  const [selected, setSelected] = useState(options[0]);
 
   const openOptionsList = () => {
     setIsOptionsList(true);
@@ -16,31 +17,36 @@ const FormikSelect = ({ title, options, selected, select }) => {
   };
 
   const handleClick = (event) => {
-    select(event.target.value);
+    const newSelected = event.target.value;
+    setSelected(newSelected);
+    select(newSelected);
     setIsOptionsList(false);
   };
 
   return (
-    <div className={styles.FilterSelect}>
+    <div className={styles.FilterSelect} style={{ width }}>
       <h3 className={styles.FilterSelectHeader}>{title}</h3>
       <div className={styles.SelectedItemContainer}>
         <p className={styles.SelectedItem}>{selected}</p>
         <button
           type="button"
           onClick={isOptionsList ? closeOptionsList : openOptionsList}
+          className={styles.OptionListButton}
         >
           <SVGImage name={isOptionsList ? "up" : "down"} size={20} />
         </button>
       </div>
       {isOptionsList && (
-        <ul className={styles.OptionsList}>
+        <ul className={styles.OptionList}>
           {options.map((option, index) => (
             <li key={index} className={styles.OptionItem}>
               <button
                 type="button"
                 onClick={handleClick}
-                className={styles.OptionButton}
                 value={option}
+                className={`${styles.OptionItemButton} ${
+                  selected === option ? styles.Selected : styles.NotSelected
+                }`}
               >
                 {option}
               </button>
@@ -52,4 +58,4 @@ const FormikSelect = ({ title, options, selected, select }) => {
   );
 };
 
-export { FormikSelect };
+export { FilterSelect };
