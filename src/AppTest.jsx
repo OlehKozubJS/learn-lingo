95a7be6940191a2cda2a84cf3f462a66e092f74e
OlehKozubJS <oleg.kozub54@gmail.com>
2024-01-27 16:11:29 +0200

    Update AppTest.jsx

--- src/AppTest.jsx
+++ src/AppTest.jsx
@@ -3,20 +3,11 @@ import { useState } from "react";
 import { TeacherFilter } from "./components/TeacherFilter";
 
 import AppStyles from "./App.module.css";
-import { SVGImage } from "./icons";
 
 function AppTest() {
-  const [selectedItems, setSelectedItems] = useState({});
-
-  const selectItems = (items) => {
-    setSelectedItems(items);
-  };
-
   return (
     <div>
       <h1>Test Page</h1>
-      <div>{JSON.stringify(selectedItems)}</div>
-      <TeacherFilter onChange={selectItems} />
     </div>
   );
 }
