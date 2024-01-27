048926b82d6c315fe71077d60240de3a2df145f3
OlehKozubJS <oleg.kozub54@gmail.com>
2024-01-27 17:59:05 +0200

    Update TeacherCard.jsx

--- src/components/TeacherCard/TeacherCard.jsx
+++ src/components/TeacherCard/TeacherCard.jsx
@@ -22,28 +22,30 @@ const TeacherCard = ({ teacherData, bookTrialLesson }) => {
   };
 
   return (
-    <article className={styles.TeacherCard}>
-      <div className={styles.TeacherImageContainer}>
-        <img
-          src={teacherData.avatar_url}
-          alt=""
-          className={styles.TeacherImage}
-        />
-      </div>
-      <div className={styles.TeacherTextInfo}>
-        <div></div>
-        <div>
-          <div></div>
-          {isReadMore && <div></div>}
+    <li>
+      <article className={styles.TeacherCard}>
+        <div className={styles.TeacherImageContainer}>
+          <img
+            src={teacherData.avatar_url}
+            alt=""
+            className={styles.TeacherImage}
+          />
         </div>
-        {isReadMore && <div></div>}
-        <button onClick={isReadMore ? hideInfo : showInfo}>
-          {isReadMore ? "Hide" : "Read More"}
-        </button>
-        <div></div>
-        <button onClick={handleBookTrialLesson}>Book trial lesson</button>
-      </div>
-    </article>
+        <div className={styles.TeacherTextInfo}>
+          <div></div>
+          <div>
+            <div></div>
+            {isReadMore && <div></div>}
+          </div>
+          {isReadMore && <div></div>}
+          <button onClick={isReadMore ? hideInfo : showInfo}>
+            {isReadMore ? "Hide" : "Read More"}
+          </button>
+          <div></div>
+          <button onClick={handleBookTrialLesson}>Book trial lesson</button>
+        </div>
+      </article>
+    </li>
   );
 };
 
