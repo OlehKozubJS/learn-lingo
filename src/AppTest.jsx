9e58269c07c531731053aa2d8e553ccdc31a97b5
OlehKozubJS <oleg.kozub54@gmail.com>
2024-01-28 19:45:56 +0200

    Update AppTest.jsx

--- src/AppTest.jsx
+++ src/AppTest.jsx
@@ -2,24 +2,43 @@ import { useState } from "react";
 
 import teachers from "./teachers.json";
 
-import { TeacherCard } from "./components";
+import { TeacherCard, LessonModal } from "./components";
 
 import AppStyles from "./App.module.css";
 
 function AppTest() {
   const [teacherData, setTeacherData] = useState({});
+  const [lessonModalData, setLessonModalData] = useState({});
+  const [isLessonModal, setIsLessonModal] = useState(false);
 
   const bookTrialLesson = (newTeacherData) => {
     setTeacherData(newTeacherData);
+    setIsLessonModal(true);
+  };
+
+  const closeLessonModal = () => {
+    setIsLessonModal(false);
+  };
+
+  const handleLessonModalSubmit = (data) => {
+    setLessonModalData(data);
   };
 
   return (
     <div>
       <h1>Test Page</h1>
-      <div>{JSON.stringify(teacherData)}</div>
+      <div>{JSON.stringify(lessonModalData)}</div>
+      {isLessonModal && (
+        <LessonModal
+          closeModal={closeLessonModal}
+          onSubmit={handleLessonModalSubmit}
+          teacherName={`${teacherData.name} ${teacherData.surname}`}
+          teacherPhoto={teacherData.avatar_url}
+        />
+      )}
       <ul>
         <TeacherCard
-          teacherData={teachers[1]}
+          teacherData={teachers[10]}
           bookTrialLesson={bookTrialLesson}
         />
       </ul>
