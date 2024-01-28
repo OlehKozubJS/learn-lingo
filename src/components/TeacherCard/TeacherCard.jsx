cf1ec12d376b9b49fb692708a4fe6cf819b17ea9
OlehKozubJS <oleg.kozub54@gmail.com>
2024-01-28 19:51:10 +0200

    Update TeacherCard.jsx

--- src/components/TeacherCard/TeacherCard.jsx
+++ src/components/TeacherCard/TeacherCard.jsx
@@ -24,7 +24,7 @@ const TeacherCard = ({ teacherData, bookTrialLesson }) => {
   };
 
   return (
-    <li>
+    <li className={styles.TeacherCardContainer}>
       <article className={styles.TeacherCard}>
         <div className={styles.TeacherImageContainer}>
           <img
