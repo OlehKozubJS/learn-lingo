4a3da55351735d32e3e625041e479371d947277a
OlehKozubJS <oleg.kozub54@gmail.com>
2024-01-28 17:37:52 +0200

    set TeacherCard

--- src/components/TeacherCard/TeacherCard.jsx
+++ src/components/TeacherCard/TeacherCard.jsx
@@ -37,9 +37,65 @@ const TeacherCard = ({ teacherData, bookTrialLesson }) => {
           </div>
         </div>
         <div className={styles.TeacherTextInfo}>
-          <div className={styles.CommonInfo}></div>
+          <div className={styles.CommonInfo}>
+            <div className={styles.TeacherNameBlock}>
+              <div className={styles.LanguagesLine}>Languages</div>
+              <div className={styles.TeacherNameLine}>
+                <span>{teacherData.name}</span>{" "}
+                <span>{teacherData.surname}</span>
+              </div>
+            </div>
+            <div className={styles.CommonInfoLineAndFavourite}>
+              <div className={styles.CommonInfoLine}>
+                <div className={styles.CommonInfoItem}>
+                  <SVGImage name="book" size={16} />
+                  Lessons online
+                </div>
+                <SVGImage name="vector" size={16} />
+                <div className={styles.CommonInfoItem}>
+                  Lessons done: {teacherData.lessons_done}
+                </div>
+                <SVGImage name="vector" size={16} />
+                <div className={styles.CommonInfoItem}>
+                  <SVGImage name="star" size={16} />
+                  Rating: {teacherData.rating}
+                </div>
+                <SVGImage name="vector" size={16} />
+                <div className={styles.CommonInfoItem}>
+                  Price / 1 hour:
+                  <span className={styles.PricePerHour}>
+                    {teacherData.price_per_hour} $
+                  </span>
+                </div>
+              </div>
+              <SVGImage name="heart" size={26} />
+            </div>
+          </div>
           <div className={styles.DetailedInfo}>
-            <div></div>
+            <div className={styles.LessonsInfo}>
+              <div className={styles.LessonsInfoItem}>
+                <span className={styles.LessonsInfoProperty}>Speaks: </span>
+                <span
+                  className={`${styles.CommonInfoValue} ${styles.Languages}`}
+                >
+                  {teacherData.languages.join(", ")}
+                </span>
+              </div>
+              <div className={styles.LessonsInfoItem}>
+                <span className={styles.LessonsInfoProperty}>
+                  Lesson Info:{" "}
+                </span>
+                <span className={styles.CommonInfoValue}>
+                  {teacherData.lesson_info}
+                </span>
+              </div>
+              <div className={styles.LessonsInfoItem}>
+                <span className={styles.LessonsInfoProperty}>Conditions: </span>
+                <span className={styles.CommonInfoValue}>
+                  {teacherData.conditions}
+                </span>
+              </div>
+            </div>
             {isReadMore && (
               <p className={styles.Experience}>{teacherData.experience}</p>
             )}
