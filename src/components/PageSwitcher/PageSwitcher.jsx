caccafdfac2edc1a15169cb267d3ccc8a123006b
OlehKozubJS <oleg.kozub54@gmail.com>
2024-01-30 23:57:50 +0200

    Update PageSwitcher.jsx

--- src/components/PageSwitcher/PageSwitcher.jsx
+++ src/components/PageSwitcher/PageSwitcher.jsx
@@ -1,13 +1,36 @@
-const PageSwitcher = ({}) => {
+import { useState } from "react";
+
+const PageSwitcher = ({ length, onChange }) => {
+  const [pageNumber, setPageNumber] = useState(0);
+
+  const firstPage = () => {};
+  const previousPage = () => {};
+  const changePageNumber = (event) => {
+    setPageNumber(event.target.value);
+  };
+  const enterPageNumber = () => {};
+  const nextPage = () => {};
+  const lastPage = () => {};
+
   return (
     <div>
       <div>
-        <button>First page</button>
-        <button>Previous page</button>
-        <input type="text" />
-        <button>Enter page number</button>
-        <button>Next page</button>
-        <button>Last page</button>
+        <button type="button" onClick={firstPage}>
+          First page
+        </button>
+        <button type="button" onClick={previousPage}>
+          Previous page
+        </button>
+        <input type="text" onChange={changePageNumber} value={pageNumber} />
+        <button type="button" onClick={enterPageNumber}>
+          Enter page number
+        </button>
+        <button type="button" onClick={nextPage}>
+          Next page
+        </button>
+        <button type="button" onClick={lastPage}>
+          Last page
+        </button>
       </div>
       <p>Page 0/0</p>
     </div>
