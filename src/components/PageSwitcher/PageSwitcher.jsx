76e0e645e8b3adaa52518b97fe1490030da1deb6
OlehKozubJS <oleg.kozub54@gmail.com>
2024-01-30 23:44:02 +0200

    Update PageSwitcher.jsx

--- src/components/PageSwitcher/PageSwitcher.jsx
+++ src/components/PageSwitcher/PageSwitcher.jsx
@@ -9,7 +9,7 @@ const PageSwitcher = ({}) => {
         <button>Next page</button>
         <button>Last page</button>
       </div>
-      <div>Page 0/0</div>
+      <p>Page 0/0</p>
     </div>
   );
 };
