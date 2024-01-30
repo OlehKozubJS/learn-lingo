897cbfa9cabd3463879be6c0eee6c6e6baa5ef4d
OlehKozubJS <oleg.kozub54@gmail.com>
2024-01-31 00:29:57 +0200

    Update PageSwitcher.jsx

--- src/components/PageSwitcher/PageSwitcher.jsx
+++ src/components/PageSwitcher/PageSwitcher.jsx
@@ -1,29 +1,38 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+// const [pageAmount, setPageAmount] = useState(Math.ceil(array.length / perPage));
+// array.slice(pageNumber, pageNumber + perPage)
 
 const PageSwitcher = ({ array, perPage, onChange }) => {
-  const [pageAmount, setPageAmount] = useState(
-    Math.ceil(array.length / perPage)
-  );
   const [pageNumber, setPageNumber] = useState(0);
+  const [newPageNumber, setNewPageNumber] = useState(0);
 
   const firstPage = () => {
     setPageNumber(0);
   };
+
   const previousPage = () => {
     setPageNumber(pageNumber - 1);
   };
+
   const changePageNumber = (event) => {
-    setPageNumber(event.target.value);
+    setNewPageNumber(event.target.value);
   };
+
   const enterPageNumber = () => {
-    onChange(array.slice(pageNumber, pageNumber + perPage));
+    setPageNumber(newPageNumber);
   };
+
   const nextPage = () => {
     setPageNumber(pageNumber + 1);
   };
+
   const lastPage = () => {
-    setPageNumber(pageAmount);
+    setPageNumber(array.length);
   };
+
+  useEffect(() => {
+    onChange(pageNumber);
+  }, [pageNumber]);
 
   return (
     <div>
@@ -34,7 +43,7 @@ const PageSwitcher = ({ array, perPage, onChange }) => {
         <button type="button" onClick={previousPage}>
           Previous page
         </button>
-        <input type="text" onChange={changePageNumber} value={pageNumber} />
+        <input type="text" onChange={changePageNumber} value={newPageNumber} />
         <button type="button" onClick={enterPageNumber}>
           Enter page number
         </button>
