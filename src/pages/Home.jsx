eabdaacc0bbd8093ff48a5faf1d42d4b3ec71e15
OlehKozubJS <oleg.kozub54@gmail.com>
2024-01-31 23:51:21 +0200

    Update Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+
+import teachers from "./teachers.json";
+
+import {
+  TeacherCards,
+  LessonModal,
+  TeacherFilter,
+  PageSwitcher,
+} from "./components";
+//import { PageSwitcherTest } from "./PageSwitcherTest";
+
+import AppStyles from "./App.module.css";
+
+function HomePage() {
+  const [keyName, setKeyName] = useState("");
+
+  useEffect(() => {
+    const handleKeydown = (event) => {
+      setKeyName(event.key);
+    };
+
+    window.addEventListener("keydown", handleKeydown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeydown);
+    };
+  }, []);
+
+  return (
+    <main>
+      <h1>Test Page</h1>
+      <div></div>
+    </main>
+  );
+}
+
+export default HomePage;
