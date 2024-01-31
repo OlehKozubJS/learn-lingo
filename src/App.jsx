f89b96bbbf63fae31f094d19690341739f924343
OlehKozubJS <oleg.kozub54@gmail.com>
2024-01-31 23:49:25 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-//import { useState } from "react";
+import { useState } from "react";
 import TeachersPage from "./pages/Teachers";
+import { AppTest } from "./AppTest";
 
 import AppStyles from "./App.module.css";
 
 function App() {
+  const [pageName, setPageName] = useState();
+
   return (
     <div>
       <h1>Main Page</h1>
       <TeachersPage />
+      <AppTest />
     </div>
   );
 }
