38967993afc2f063b21b96a1009b3d7c87b79669
OlehKozubJS <oleg.kozub54@gmail.com>
2024-01-31 23:41:42 +0200

    Update AppTest.jsx

--- src/AppTest.jsx
+++ src/AppTest.jsx
@@ -13,43 +13,9 @@ import {
 import AppStyles from "./App.module.css";
 
 function AppTest() {
-  const [filteredTeachers, setFilteredTeachers] = useState([...teachers]);
-  const [teachersOnPage, setTeachersOnPage] = useState([]);
-
-  const handlePageSwitcherChange = (data) => {
-    setTeachersOnPage(data);
-  };
-
-  const handleTeacherFilterChange = ({ language, level, price }) => {
-    const filter = teachers.filter(
-      (teacher) =>
-        (language === "any language" || teacher.languages.includes(language)) &&
-        (level === "any level" || teacher.levels.includes(level)) &&
-        (price === "any price" ||
-          (Number(teacher.price_per_hour) >= Number(price.split(" ")[0]) &&
-            Number(teacher.price_per_hour) < Number(price.split(" ")[0]) + 10))
-    );
-    setFilteredTeachers(filter);
-  };
-
   return (
     <main>
       <h1>Test Page</h1>
-      <PageSwitcher
-        list={filteredTeachers}
-        onChange={handlePageSwitcherChange}
-        perPage={3}
-      />
-      <TeacherFilter onChange={handleTeacherFilterChange} />
-      <ol style={{ display: "flex", flexWrap: "wrap", gap: "20 px 10px" }}>
-        {teachersOnPage.map((item, index) => {
-          return (
-            <li key={index} style={{ width: "200px" }}>
-              <span>{item.name}</span> <span>{item.surname}</span>
-            </li>
-          );
-        })}
-      </ol>
     </main>
   );
 }
