9323ce44594922837a5bcc16b413a8b9c8172fe0
OlehKozubJS <oleg.kozub54@gmail.com>
2024-01-31 13:53:30 +0200

    Update PageSwitcherTest.jsx

--- src/PageSwitcherTest.jsx
+++ src/PageSwitcherTest.jsx
@@ -4,10 +4,26 @@ const PageSwitcherTest = ({ list, perPage }) => {
   const [amount, setAmount] = useState(Math.ceil(list, perPage));
   const [number, setNumber] = useState(1);
 
+  const toPreviousPage = () => {
+    setNumber(number - 1);
+  };
+
+  const toNextPage = () => {
+    setNumber(number + 1);
+  };
+
   return (
     <div>
-      {number !== 1 && <button>Previous</button>}
-      {number !== amount && <button>Next</button>}
+      {number !== 1 && (
+        <button type="button" onClick={toPreviousPage}>
+          Previous
+        </button>
+      )}
+      {number !== amount && (
+        <button type="button" onClick={toNextPage}>
+          Next
+        </button>
+      )}
       <div>Page: {number}</div>
     </div>
   );
