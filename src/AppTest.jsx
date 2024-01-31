eecfec31bf70ca0af36a5ebb4a0a756821836f4f
OlehKozubJS <oleg.kozub54@gmail.com>
2024-01-31 23:46:11 +0200

    Update AppTest.jsx

--- src/AppTest.jsx
+++ src/AppTest.jsx
@@ -13,9 +13,24 @@ import {
 import AppStyles from "./App.module.css";
 
 function AppTest() {
+  const [keyName, setKeyName] = useState("");
+
+  useEffect(() => {
+    const handleKeydown = (event) => {
+      setKeyName(event.key);
+    };
+
+    window.addEventListener("keydown", handleKeydown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeydown);
+    };
+  }, []);
+
   return (
     <main>
       <h1>Test Page</h1>
+      <div></div>
     </main>
   );
 }
