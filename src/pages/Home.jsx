773d3d62084e9c362fcbf7fa399248f2340325d3
OlehKozubJS <oleg.kozub54@gmail.com>
2024-01-31 23:52:54 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,16 @@
 import { useState, useEffect } from "react";
 
-import teachers from "./teachers.json";
+import teachers from "../teachers.json";
 
 import {
   TeacherCards,
   LessonModal,
   TeacherFilter,
   PageSwitcher,
-} from "./components";
+} from "../components";
 //import { PageSwitcherTest } from "./PageSwitcherTest";
 
-import AppStyles from "./App.module.css";
+import AppStyles from "../App.module.css";
 
 function HomePage() {
   const [keyName, setKeyName] = useState("");
