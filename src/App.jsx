a26eee83da1b0778cc2348f8461b1a7ee0250c06
OlehKozubJS <oleg.kozub54@gmail.com>
2024-02-01 16:37:23 +0200

    set header styles

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import HomePage from "./pages/Home";
 import TeachersPage from "./pages/Teachers";
 import { AppTest } from "./AppTest";
 
-import AppStyles from "./App.module.css";
+import styles from "./App.module.css";
 
 function App() {
   const [pageName, setPageName] = useState();
@@ -16,14 +16,29 @@ function App() {
   return (
     <div>
       <header>
-        <nav>
-          <button type="button" onClick={getPage} value="home">
+        <nav className={styles.NavigationBar}>
+          <button
+            type="button"
+            onClick={getPage}
+            value="home"
+            className={styles.NavigationButton}
+          >
             Home
           </button>
-          <button type="button" onClick={getPage} value="teachers">
+          <button
+            type="button"
+            onClick={getPage}
+            value="teachers"
+            className={styles.NavigationButton}
+          >
             Teachers
           </button>
-          <button type="button" onClick={getPage} value="test">
+          <button
+            type="button"
+            onClick={getPage}
+            value="test"
+            className={styles.NavigationButton}
+          >
             Test Page
           </button>
         </nav>
