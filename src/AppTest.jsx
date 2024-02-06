190226fce61bac4b59bcb43eabc0ed183a7f0c7c
OlehKozubJS <oleg.kozub54@gmail.com>
2024-02-06 02:22:22 +0200

    Update AppTest.jsx

--- src/AppTest.jsx
+++ src/AppTest.jsx
@@ -67,7 +67,7 @@ function AppTest() {
   useEffect(() => {
     const getDataFromBackend = async () => {
       const response = await axios.get("http://localhost:3000/load");
-      setDataFromBackend(JSON.stringify(response));
+      setDataFromBackend(JSON.stringify(response.data));
     };
     getDataFromBackend();
   }, []);
