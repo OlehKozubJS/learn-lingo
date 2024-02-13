2d945753a7d8963dfbed058161221f3415891234
OlehKozubJS <oleg.kozub54@gmail.com>
2024-02-13 22:57:07 +0200

    Create FormikModalRadio.jsx

---
+++ src/formik-components/FormikModalRadio.jsx
@@ -0,0 +1,36 @@
+import { SVGImage } from "../icons";
+
+import styles from "./FormikRadio.module.css";
+
+const FormikRadio = ({ formik, title, name, options }) => {
+  return (
+    <div className={styles.FormikRadio}>
+      <h3 className={styles.FormikRadioHeader}>{title}</h3>
+      <ul className={styles.FormikRadioList}>
+        {options.map((option, index) => (
+          <li key={index} className={styles.FormikRadioItem}>
+            <label htmlFor={option} className={styles.FormikRadioLabel}>
+              <input
+                id={option}
+                name={name}
+                type="radio"
+                onChange={formik.handleChange}
+                value={option}
+                className={styles.FormikRadioButton}
+              />
+              <SVGImage
+                name={
+                  formik.values[name] === option ? "radio-checked" : "radio"
+                }
+                size={20}
+              />
+              <p className={styles.FormikRadioText}>{option}</p>
+            </label>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export { FormikRadio };
