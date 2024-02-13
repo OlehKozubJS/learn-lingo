cdfe653005bd5cc93f8cee271c278222328f0b36
OlehKozubJS <oleg.kozub54@gmail.com>
2024-02-13 23:48:29 +0200

    Update FormikRadio.jsx

--- src/formik-components/FormikRadio.jsx
+++ src/formik-components/FormikRadio.jsx
@@ -2,34 +2,25 @@ import { SVGImage } from "../icons";
 
 import styles from "./FormikRadio.module.css";
 
-const FormikRadio = ({ formik, title, name, options }) => {
+const FormikRadio = ({ formik, name, options, children, styles }) => {
   return (
-    <div className={styles.FormikRadio}>
-      <h3 className={styles.FormikRadioHeader}>{title}</h3>
-      <ul className={styles.FormikRadioList}>
-        {options.map((option, index) => (
-          <li key={index} className={styles.FormikRadioItem}>
-            <label htmlFor={option} className={styles.FormikRadioLabel}>
-              <input
-                id={option}
-                name={name}
-                type="radio"
-                onChange={formik.handleChange}
-                value={option}
-                className={styles.FormikRadioButton}
-              />
-              <SVGImage
-                name={
-                  formik.values[name] === option ? "radio-checked" : "radio"
-                }
-                size={20}
-              />
-              <p className={styles.FormikRadioText}>{option}</p>
-            </label>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <ul className={styles.FormikRadioList}>
+      {options.map((option, index) => (
+        <li key={index} className={styles.FormikRadioItem}>
+          <label htmlFor={option} className={styles.FormikRadioLabel}>
+            <input
+              id={option}
+              name={name}
+              type="radio"
+              onChange={formik.handleChange}
+              value={option}
+              className={styles.FormikRadioButton}
+            />
+            <div>{children}</div>
+          </label>
+        </li>
+      ))}
+    </ul>
   );
 };
 
