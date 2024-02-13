5443ed5b49a23afdccdb62d91e927775b0cd4b10
OlehKozubJS <oleg.kozub54@gmail.com>
2024-02-14 00:02:42 +0200

    Update FormikRadio.jsx

--- src/formik-components/FormikRadio.jsx
+++ src/formik-components/FormikRadio.jsx
@@ -2,12 +2,12 @@ import { SVGImage } from "../icons";
 
 import styles from "./FormikRadio.module.css";
 
-const FormikRadio = ({ formik, name, options, children, styles }) => {
+const FormikRadio = ({ formik, name, options, children, style }) => {
   return (
-    <ul className={styles.FormikRadioList}>
+    <ul className={styles.FormikRadio}>
       {options.map((option, index) => (
         <li key={index} className={styles.FormikRadioItem}>
-          <label htmlFor={option} className={styles.FormikRadioLabel}>
+          <label htmlFor={option}>
             <input
               id={option}
               name={name}
