1ef0e81ad49fff45f2de67fe02f41e0c1fe6d683
OlehKozubJS <oleg.kozub54@gmail.com>
2024-03-18 22:51:37 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+
+import {} from "./";
+
 import { App } from "./App.jsx";
-import { AppTest } from "./pages/AppTestPage.jsx";
 
 const root = document.querySelector("#root");
 const reactRoot = createRoot(root);
 
 reactRoot.render(
   <StrictMode>
-    <BrowserRouter basename="/learn-lingo">
-      <App />
-    </BrowserRouter>{" "}
+    <Provider>
+      <BrowserRouter basename="/learn-lingo">
+        <App />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>
 );
