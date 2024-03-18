716cf621d5e6c13d99fa948e5307371d07785645
OlehKozubJS <oleg.kozub54@gmail.com>
2024-03-18 22:25:37 +0200

    Update teachersSlice.js

--- src/redux/teachersSlice.js
+++ src/redux/teachersSlice.js
@@ -1,8 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const handlePending = (state) => {
+  state.isLoading = true;
+};
+
+const handleRejected = (state, action) => {
+  state.isLoading = false;
+  state.error = action.payload;
+};
+
 const teachersSlice = createSlice({
   name: "teachers",
-  initialState,
+  initialState: {
+    teachersList: [],
+    isLoading: false,
+    error: null,
+  },
   extraReducers,
 });
 
