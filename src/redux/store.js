5e938f94033bb3d3f247d96d4798e6a44a513477
OlehKozubJS <oleg.kozub54@gmail.com>
2024-03-18 22:50:07 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,3 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import { persistReducers, middleware, persistStore } from "./persistor";
+
+import { teachers } from "./teachersSlice";
+
+const store = configureStore({ reducer: { teachers }, middleware });
+
+export { store };
