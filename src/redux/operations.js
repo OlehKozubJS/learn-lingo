ca2eac18869b8058112147be278a5452ab6944a7
OlehKozubJS <oleg.kozub54@gmail.com>
2024-03-18 22:40:06 +0200

    Update operations.js

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,6 +1,20 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
 const getTeachers = createAsyncThunk(
-  "",
-  ({ language, level, price, page }, thunkAPI) => {}
+  "teachers/getTeachers",
+  async ({ language, level, price, page }, thunkAPI) => {
+    try {
+      const response = await axios.get(
+        `http://localhost:3000/load/` +
+          `?language=${language || "any language"}` +
+          `&level=${level || "any level"}` +
+          `&price=${price || "any price"}` +
+          `&page=${page || 1}` +
+          `&perPage=3`
+      );
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
 );
