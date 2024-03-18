5d8cc5e834587c4471a99110ab99c6fa844315ee
OlehKozubJS <122114245+OlehKozubJS@users.noreply.github.com>
2024-03-18 21:04:55 +0000

    Deploying to gh-pages from @ OlehKozubJS/learn-lingo@e256c5143642066f5f5dfdecfc4581a4e4672f47 🚀

--- static/js/808.6a58472c.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunklearn_lingo=self.webpackChunklearn_lingo||[]).push([[808],{983:(e,n,s)=>{s.r(n),s.d(n,{default:()=>i});var t=s(791),r=(s(455),s(109),s(160),s(184));const i=function(){const[e,n]=(0,t.useState)("");return(0,t.useEffect)((()=>{const e=e=>{n(e.key)};return window.addEventListener("keydown",e),()=>{window.removeEventListener("keydown",e)}}),[]),(0,r.jsxs)("main",{children:[(0,r.jsx)("h1",{children:"Home Page"}),(0,r.jsx)("div",{})]})}}}]);
-//# sourceMappingURL=808.6a58472c.chunk.js.map
