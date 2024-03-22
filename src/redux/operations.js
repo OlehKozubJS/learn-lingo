be69ce20395c9ce01cd0cb6956165bb0e97df7c8
OlehKozubJS <oleg.kozub54@gmail.com>
2024-03-22 23:58:12 +0200

    Update operations.js

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 import { createAsyncThunk } from "@reduxjs/toolkit";
-
+/*
 const createThunk = (reducerName, thunkName, thunk) => {
   return createAsyncThunk(
     `${reducerName}/${thunkName}`,
@@ -14,9 +14,9 @@ const createThunk = (reducerName, thunkName, thunk) => {
     }
   );
 };
-
+*/
 const getTeachersThunk = async (params) => {
-  const { language, level, price, page } = params;
+  const { language, level, price, page } = await params;
   const response = await axios.get(
     `http://localhost:3000/load/` +
       `?language=${language || "any language"}` +
@@ -27,7 +27,30 @@ const getTeachersThunk = async (params) => {
   );
   return response.data;
 };
-
+/*
 const getTeachers = createThunk("teachers", "getTeachers", getTeachersThunk);
+*/
+const getTeachers = createAsyncThunk(
+  `teachers/getTeachers`,
+  async (params, thunkAPI) => {
+    try {
+      const getTeachersThunk = async (params) => {
+        const { language, level, price, page } = await params;
+        const response = await axios.get(
+          `http://localhost:3000/load/` +
+            `?language=${language || "any language"}` +
+            `&level=${level || "any level"}` +
+            `&price=${price || "any price"}` +
+            `&page=${page || 1}` +
+            `&perPage=3`
+        );
+        return response.data;
+      };
+      await getTeachersThunk(params);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
 
 export { getTeachers };
