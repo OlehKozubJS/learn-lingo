09f15a383f1bda3880e43b1652d2f4b5b02fb203
OlehKozubJS <oleg.kozub54@gmail.com>
2024-04-28 13:25:16 +0300

    Update operations.js

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,35 +1,7 @@
 import axios from "axios";
 
 import { createAsyncThunk } from "@reduxjs/toolkit";
-/*
-const createThunk = (reducerName, thunkName, thunk) => {
-  return createAsyncThunk(
-    `${reducerName}/${thunkName}`,
-    async (params, thunkAPI) => {
-      try {
-        await thunk(params);
-      } catch (error) {
-        return thunkAPI.rejectWithValue(error);
-      }
-    }
-  );
-};
 
-const getTeachersThunk = async (params) => {
-  const { language, level, price, page } = await params;
-  const response = await axios.get(
-    `http://localhost:3000/load/` +
-      `?language=${language || "any language"}` +
-      `&level=${level || "any level"}` +
-      `&price=${price || "any price"}` +
-      `&page=${page || 1}` +
-      `&perPage=3`
-  );
-  return response.data;
-};
-
-const getTeachers = createThunk("teachers", "getTeachers", getTeachersThunk);
-*/
 const getTeachers = createAsyncThunk(
   `teachers/getTeachers`,
   async (params, thunkAPI) => {
